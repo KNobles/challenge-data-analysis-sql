1f1d4a40aed21c093b1089f99d678d79884dd796
KNobles <nobles-k@hotmail.com>
2022-10-26 14:30:21 +0200

    pie chart works

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sqlite3
 import pandas as pd
 import streamlit as st
 import plotly.express as px
-
+import plotly.graph_objects as go
 
 def create_connection(db_file: str):
     sqlite_connection = None
@@ -39,7 +39,8 @@ def run_query():
 
 sqlite_connection = sqlite3.connect("bce.db")
 
-def form_percentage():
+def get_form():
+    st.markdown("# Percentage of juridical form in enterprises")
     cursor = create_connection("bce.db").cursor()
     form_list = cursor.execute("""
             SELECT form,SUM(count) count
@@ -55,18 +56,22 @@ def form_percentage():
             """)
     cols = [column[0] for column in form_list.description]
     forms = form_list.fetchall()
-    forms.remove(forms[0])
-
     df_form = pd.DataFrame.from_records(data=forms, columns=cols)
-
-    fig = px.pie(df_form, values='count', names='form', title='Percentage of form')
+    df_form.loc[df_form["form"].astype(str) == "None", "form"] = "Null"
+    # print(df_form["form"].values)
+    # fig = px.pie(df_form, values='count', names='form', title='Percentage of form')
+    pull_arr = [0] * len(df_form["form"].array)
+    # print(pull_arr)
+    # print(df_form.loc[(df_form["count"] == df_form["count"].max()) & (df_form["form"] != "Null")])
+    print(df_form.max())
+    fig = go.Figure(data=[go.Pie(labels=df_form['form'].values, values=df_form['count'].values, pull=[0,0,0,0,0,0,0.1,0])])
     st.plotly_chart(fig)
 
 # st.bar_chart(data=recs, x="form", y="count")
 
 pages = {
     "Run Query" : run_query(),
-    "Forms percentages" : form_percentage(),
+    "Forms percentages" : get_form(),
 }
 
 selected_page = st.sidebar.selectbox("Select a page", pages.keys())
